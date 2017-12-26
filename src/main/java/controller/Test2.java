6590e1029b505c8f9a11b922c30c394946c44785
XJG123456 <370326692@qq.com>
2017-12-26 10:11:30 +0800

    提一次

--- src/main/java/controller/Test2.java
+++ src/main/java/controller/Test2.java
@@ -1,21 +1,12 @@
 package controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		Map<Integer, String> sMap=new HashMap<>();
-		long star=System.currentTimeMillis();
-		for(int i=0;i<500;i++){
-		sMap.put(1, "o");
-		sMap.put(2, "p");
-		sMap.put(3, "w");
-		}
-		for(Map.Entry<Integer, String> entry:sMap.entrySet()){
-			System.out.println("key:"+entry.getKey()+"value:"+entry.getValue());
-		}
-		System.out.println(System.currentTimeMillis()-star);
+		int nubmer=30;
+		int b=30;
+		System.out.println(Objects.requireNonNull(nubmer));
 	}
 }
